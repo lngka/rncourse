fc9a6fde3480fa2ae1c502ca5cc401b685fd4354
lngka <foxlead@gmail.com>
2019-03-27 19:08:23 +0100

    updating redux

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
+import configureStore from './src/store/configureStore';
 
+const store = configureStore();
 
 // registering screen...
-Navigation.registerComponent('rncourse.AuthScreen', () => AuthScreen);
-Navigation.registerComponent('rncourse.SharePlaceScreen', () => SharePlaceScreen);
-Navigation.registerComponent('rncourse.FindPlaceScreen', () => FindPlaceScreen);
+Navigation.registerComponent('rncourse.AuthScreen', () => AuthScreen, store, Provider);
+Navigation.registerComponent('rncourse.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
+Navigation.registerComponent('rncourse.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
 
 
 
--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,14 +1,27 @@
 import React,  { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+
+import { addPlace } from '../../store/actions/index';
+import PlaceInput from '../../components/PlaceInput/PlaceInput';
 
 class SharePlaceScreen extends Component {
+    onPlaceAddedHandler = placeName => {
+        return this.props.onAddPlace(placeName);
+    }
     render () {
         return (
             <View>
-                <Text> on SharePlaceScreen</Text>
+                <PlaceInput onPlaceAdded={this.onPlaceAddedHandler}/>
             </View>
         );
     }
 }
 
-export default SharePlaceScreen;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddPlace: (placename) => dispatch(addPlace(placename))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(SharePlaceScreen);
